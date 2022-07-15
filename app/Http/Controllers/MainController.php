bb58c6e6fcfaa9bbd605c393b2e4f9c2572b38a4
Rahman <obitel_zero@protonmail.com>
2022-07-15 15:50:39 +0300

    Feature: possible read messages

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,12 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
 final class MainController
 {
 
-    public function index()
+    public function index(Request $request)
     {
+        $perPage = $request->get('per_page');
 
+        $messages = Message::query()
+            ->paginate($perPage);
+
+        if ($messages->isEmpty()) {
+            return new JsonResponse(['code' => 200, 'message' => 'Нет сообщений']);
+        }
+
+        return new JsonResponse(['code' => 200, 'message' => $messages->toArray()]);
     }
 
 }
